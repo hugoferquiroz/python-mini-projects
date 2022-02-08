1e551583a7348db4564b2ccfa3f03b1d68e5d818
Hugo <hugofernandezquiroz@gmail.com>
2022-02-07 22:48:27 -0500

    Update Palindromes.py

--- Palindromes.py
+++ Palindromes.py
@@ -1,3 +1,24 @@
 #palindromes
 
-print('In progress')
+
+def is_palindrome():
+    your_word = your_words.replace(' ', '')
+    your_word = your_word.lower()
+    your_reverse_word = your_word[::-1]
+    if your_word == your_reverse_word:
+        return  True
+    else:
+        return False
+    
+
+def run():
+    your_words = input('Write your words: ')
+    palindrome = is_palindrome(your_words)
+    if is_palindrome == True:
+        print('It\'s palindrome')
+    else:
+        print('It\'s not palindrome')
+
+
+if __name__ == '__main__':
+    run()
